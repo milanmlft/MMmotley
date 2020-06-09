b02cc8555cfe89844288c1d65d579f3b96b83adc
Milan Malfait <milan.malfait94@gmail.com>
2020-06-09 18:14:19 +0200

    Test gg_pval_hist()

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(MMmotley)
+
+test_check("MMmotley")
---
+++ tests/testthat/test-plots.R
@@ -0,0 +1,12 @@
+test_that("gg_pval_hist() fails for impossible p-values", {
+	expect_error(gg_pval_hist(c(-0.1, runif(10))))
+	expect_error(gg_pval_hist(c(1.1, runif(10))))
+})
+
+test_that("Output of gg_pval_hist() is stable", {
+  # Create synthetic p-values, includes the value 0, so first bin [0, 0.05] will
+  # contain 6 values, while all others will contain 5
+  pvals <- seq(0, 1, by = 0.01)
+  vdiffr::expect_doppelganger("p-value histogram", gg_pval_hist(pvals),
+  														path = "")
+})
