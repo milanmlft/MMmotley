275f5184e7cd908fab40946a72206685513a479a
Milan Malfait <milan.malfait94@gmail.com>
2020-06-14 18:36:21 +0200

    Add save_plots()

--- R/plots.R
+++ R/plots.R
@@ -44,3 +44,38 @@ gg_pval_hist <- function(p_values) {
   	# Lets the bottoms of the histogram bars touch the x-axis
     ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0.05)))
 }
+
+
+#' Save multiple plots to a single PDF
+#'
+#' Takes a list of plots and exports them to a single PDF with one plot per page.
+#'
+#' @param plot_list List of plot objects. Can be any type as long as
+#'                  it has a `print()` output.
+#' @param filename  Character string. Can also be specified as path.
+#'
+#' @examples
+#' \dontrun{
+#'   library(ggplot2)
+#'
+#'   # Generate list of plots
+#'   mtcars_split <- split(mtcars, mtcars$cyl)
+#'   plot_list <- lapply(mtcars_split, function(d){
+#'     ggplot(d, aes(mpg, wt)) +
+#'       geom_point()
+#'   })
+#'
+#'   # Save plots
+#'   save_plots(plot_list, "test.pdf")
+#'
+#'   # Delete file with `base::unlink()`
+#'   unlink("test.pdf")
+#' }
+#'
+#' @export
+#' @importFrom grDevices pdf dev.off
+save_plots <- function(plot_list, filename) {
+  pdf(filename)
+  invisible(lapply(plot_list, print))
+  dev.off()
+}
