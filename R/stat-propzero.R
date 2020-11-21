ab2eb47fac1ee71494896cf496602b4cc40def09
Milan Malfait <milan.malfait94@gmail.com>
2020-11-21 01:53:00 +0100

    Add miminal working version of `stat_propzero

--- R/stat-propzero.R
+++ R/stat-propzero.R
@@ -7,7 +7,27 @@
 #' @export
 #'
 #' @examples
-stat_propzero <- function(mapping = NULL, ...) {
+stat_propzero <- function(mapping = NULL, data = NULL, geom = "text",
+                          position = "identity", na.rm = FALSE,
+                          inherit.aes = TRUE,
+                          ...) {
+  # TODO: replace `layer` with `annotate`?
+  # Might allow more flexible positioning of text annotation
+  ggplot2::layer(
+    stat = StatPropZero, data = data, mapping = mapping, geom = geom,
+    position = position, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
 }
 
-
+# FIXME: figure out how to supply `format` arugment to format the proportions
+# e.g. as percentages or rounded decimals
+# ggproto object for stat_propzero
+StatPropZero <- ggplot2::ggproto("StatPropZero", ggplot2::Stat,
+  required_aes = c("x", "y"),
+  compute_group = function(data, scales) {
+    grouped <- dplyr::group_by(data, x)
+    prop_zero <- dplyr::summarise(grouped, label = mean(y == 0))
+    data.frame(x = prop_zero$x, y = 0, label = prop_zero$label)
+  }
+)
