3fcd9582ff8995d3f34b3e5b1167819449b485b7
Milan Malfait <milan.malfait94@gmail.com>
2020-11-23 10:43:10 +0100

    Minor fixes and update documentation

--- R/stat-propzero.R
+++ R/stat-propzero.R
@@ -1,6 +1,6 @@
 #' Annotating ggplots with prop. of zeros
 #'
-#' @inheritParams ggplot2::stat_identity
+#' @inheritParams ggplot2::stat_count
 #' @section Computed variables:
 #' `stat_propzero()` provides the following variables:
 #' \describe{
@@ -11,21 +11,20 @@
 stat_propzero <- function(mapping = NULL, data = NULL, geom = "text",
                           position = "identity", na.rm = FALSE,
                           show.legend = NA, inherit.aes = TRUE, ...) {
-  # TODO: replace `layer` with `annotate`?
-  # Might allow more flexible positioning of text annotation
+  # TODO: find way to allow more flexible positioning of labels
   ggplot2::layer(
-    stat = StatPropZero, data = data, mapping = mapping, geom = geom,
+    stat = StatPropzero, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, ...)
   )
 }
 
-# FIXME: figure out how to supply `format` arugment to format the proportions
+# FIXME: figure out how to supply `format` argument to format the proportions
 # e.g. as percentages or rounded decimals
 # ggproto object for stat_propzero
-StatPropZero <- ggplot2::ggproto("StatPropZero", ggplot2::Stat,
+StatPropzero <- ggplot2::ggproto("StatPropzero", ggplot2::Stat,
   required_aes = c("x", "y"),
-  default_aes = aes(label = stat(propzero)),
+  default_aes = aes(label = after_stat(propzero)),
 
   compute_group = function(data, scales) {
     grouped <- dplyr::group_by(data, x)
