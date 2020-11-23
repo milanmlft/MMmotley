3fba01f44a2d26821d8cd0d262fce65b51b03203
Milan Malfait <milan.malfait94@gmail.com>
2020-11-23 11:19:57 +0100

    Add formatting function argument

--- R/stat-propzero.R
+++ R/stat-propzero.R
@@ -1,6 +1,10 @@
 #' Annotating ggplots with prop. of zeros
 #'
 #' @inheritParams ggplot2::stat_count
+#' @param format.fun Function to format labels when using `geom = "text"` (the
+#'                   default) to depict the proportion of zeros.
+#'                   By default, uses [scales::label_percent()].
+#'
 #' @section Computed variables:
 #' `stat_propzero()` provides the following variables:
 #' \describe{
@@ -8,27 +12,31 @@
 #' }
 #'
 #' @export
+
+# TODO: add examples
+
 stat_propzero <- function(mapping = NULL, data = NULL, geom = "text",
                           position = "identity", na.rm = FALSE,
-                          show.legend = NA, inherit.aes = TRUE, ...) {
-  # TODO: find way to allow more flexible positioning of labels
+                          show.legend = NA, inherit.aes = TRUE, ...,
+                          format.fun = scales::label_percent()) {
+
   ggplot2::layer(
     stat = StatPropzero, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, ...)
+    params = list(format.fun = format.fun, na.rm = na.rm, ...)
   )
 }
 
-# FIXME: figure out how to supply `format` argument to format the proportions
-# e.g. as percentages or rounded decimals
 # ggproto object for stat_propzero
 StatPropzero <- ggplot2::ggproto("StatPropzero", ggplot2::Stat,
   required_aes = c("x", "y"),
-  default_aes = aes(label = after_stat(propzero)),
 
-  compute_group = function(data, scales) {
+  compute_group = function(data, scales, format.fun = scales::label_percent()) {
     grouped <- dplyr::group_by(data, x)
     out <- dplyr::summarise(grouped, propzero = mean(y == 0))
-    data.frame(x = out$x, y = 0, propzero = out$propzero)
+    data.frame(
+      x = out$x, y = 0, propzero = out$propzero,
+      label = format.fun(out$propzero)
+    )
   }
 )
